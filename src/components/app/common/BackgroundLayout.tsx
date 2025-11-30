09949cf6d40729a1c83dcddc69e5b2bb7be57dc6
Ethan Frot <contact@ethanfrot.com>
2025-11-30 13:10:03 +0100

    🚚 Move FloatingLines into common components folder

--- src/components/app/common/BackgroundLayout.tsx
+++ src/components/app/common/BackgroundLayout.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import FloatingLines from '@/components/FloatingLines';
+import FloatingLines from "@/components/app/common/FloatingLines";
 
 /**
  * BackgroundLayout - Layout global pour le background animé
@@ -17,7 +17,7 @@ export function BackgroundLayout({ children }: { children: React.ReactNode }) {
       {/* Background animé avec FloatingLines (Three.js) */}
       <div className="fixed inset-0 z-0">
         <FloatingLines
-          enabledWaves={['top', 'middle', 'bottom']}
+          enabledWaves={["top", "middle", "bottom"]}
           lineCount={5}
           lineDistance={5}
           bendRadius={5.0}
@@ -28,9 +28,7 @@ export function BackgroundLayout({ children }: { children: React.ReactNode }) {
       </div>
 
       {/* Content avec z-index supérieur */}
-      <div className="relative z-10">
-        {children}
-      </div>
+      <div className="relative z-10">{children}</div>
     </div>
   );
 }
