8ceb59f8073ee92411da30c77155f118056a24a6
Ethan Frot <contact@ethanfrot.com>
2025-12-01 12:57:26 +0100

    ✨ US-7 : Add updateMessagePrismaRepository

---
+++ src/module/message/updateMessage/UpdateMessagePrismaRepository.ts
@@ -0,0 +1,40 @@
+import { prisma } from '@/lib/prisma';
+import { UpdateMessageRepository } from './UpdateMessageRepository';
+import { Message } from '@/domain/message/Message';
+import { PrismaClient } from '@prisma/client';
+
+export class UpdateMessagePrismaRepository implements UpdateMessageRepository {
+  private prismaClient: PrismaClient;
+
+  constructor(prismaClient?: PrismaClient) {
+    this.prismaClient = prismaClient || prisma;
+  }
+
+  async findById(id: string): Promise<Message | null> {
+    const data = await this.prismaClient.message.findUnique({
+      where: { id, deletedAt: null },
+    });
+
+    if (!data) return null;
+
+    return new Message({
+      id: data.id,
+      content: data.content,
+      authorId: data.authorId,
+      conversationId: data.conversationId,
+      createdAt: data.createdAt,
+      updatedAt: data.updatedAt,
+      deletedAt: data.deletedAt,
+    });
+  }
+
+  async update(message: Message): Promise<void> {
+    await this.prismaClient.message.update({
+      where: { id: message.id },
+      data: {
+        content: message.content,
+        updatedAt: new Date(),
+      },
+    });
+  }
+}
